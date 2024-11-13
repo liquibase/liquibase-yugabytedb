3ba3e52d1eccdb5b451703a8edd470316397a4f7
filipe <flautert@liquibase.org>
2024-11-13 10:55:14 -0300

    chore: convert test harness class to java

--- src/test/groovy/AdvancedExtensionHarnessSuite.groovy
+++
@@ -1,8 +0,0 @@
-import liquibase.harness.AdvancedHarnessSuite
-import liquibase.harness.snapshot.SnapshotObjectTests
-import org.junit.platform.suite.api.SelectClasses
-
-@SelectClasses({ SnapshotObjectTests.class})
-class AdvancedExtensionHarnessTestSuite extends AdvancedHarnessSuite {
-    
-}
---
+++ src/test/java/liquibase/ext/yugabytedb/AdvancedExtensionHarnessTestSuite.java
@@ -0,0 +1,8 @@
+package liquibase.ext.yugabytedb;
+
+import liquibase.harness.snapshot.SnapshotObjectTests;
+import org.junit.platform.suite.api.SelectClasses;
+
+@SelectClasses({ SnapshotObjectTests.class})
+public class AdvancedExtensionHarnessTestSuite {
+}
