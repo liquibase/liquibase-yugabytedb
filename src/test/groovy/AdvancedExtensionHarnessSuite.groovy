f68ad50c217fb6b4f9991dd25ce4affbb65859a3
filipe <flautert@liquibase.org>
2024-11-13 11:03:20 -0300

    chore: fix groovy class name

---
+++ src/test/groovy/AdvancedExtensionHarnessSuite.groovy
@@ -0,0 +1,6 @@
+import liquibase.harness.snapshot.SnapshotObjectTests
+import org.junit.platform.suite.api.SelectClasses
+
+@SelectClasses([SnapshotObjectTests.class])
+class AdvancedExtensionHarnessSuite {
+}
--- src/test/java/liquibase/ext/yugabytedb/AdvancedExtensionHarnessSuite.java
+++
@@ -1,8 +0,0 @@
-package liquibase.ext.yugabytedb;
-
-import liquibase.harness.snapshot.SnapshotObjectTests;
-import org.junit.platform.suite.api.SelectClasses;
-
-@SelectClasses({ SnapshotObjectTests.class})
-public class AdvancedExtensionHarnessSuite {
-}
