c1136998d788f17cae1b337662029dbe8ca5b39c
filipe <flautert@liquibase.org>
2024-11-13 10:46:44 -0300

    fix typo

--- src/test/groovy/AdvancedExtensionHarnessSuite.groovy
+++ src/test/groovy/AdvancedExtensionHarnessSuite.groovy
@@ -2,7 +2,7 @@ import liquibase.harness.AdvancedHarnessSuite
 import liquibase.harness.snapshot.SnapshotObjectTests
 import org.junit.platform.suite.api.SelectClasses
 
-@SelectClasses([SnapshotObjectTests.class])
+@SelectClasses({ SnapshotObjectTests.class})
 class AdvancedExtensionHarnessTestSuite extends AdvancedHarnessSuite {
     
 }
