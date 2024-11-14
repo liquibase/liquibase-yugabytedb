0ef3fc8cba46e4b952873f79540e0854b2e849f4
filipe <flautert@liquibase.org>
2024-11-14 10:29:33 -0300

    Fix test harness execution (#228)
    Fix test harness after bumping to new Parent pom and test harness versions.

--- src/test/groovy/AdvancedExtensionHarnessSuite.groovy
+++ src/test/groovy/AdvancedExtensionHarnessSuite.groovy
@@ -1,5 +1,7 @@
 import liquibase.harness.AdvancedHarnessSuite
+import liquibase.harness.snapshot.SnapshotObjectTests
+import org.junit.platform.suite.api.SelectClasses
 
+@SelectClasses([SnapshotObjectTests.class])
 class AdvancedExtensionHarnessSuite extends AdvancedHarnessSuite {
-    
 }
