77d569980fc20b5a9440674b6aecdf909cfa2844
filipe <flautert@liquibase.org>
2024-11-14 10:20:57 -0300

    Update AdvancedExtensionHarnessSuite.groovy

--- src/test/groovy/AdvancedExtensionHarnessSuite.groovy
+++ src/test/groovy/AdvancedExtensionHarnessSuite.groovy
@@ -3,5 +3,5 @@ import liquibase.harness.snapshot.SnapshotObjectTests
 import org.junit.platform.suite.api.SelectClasses
 
 @SelectClasses([SnapshotObjectTests.class])
-class AdvancedExtensionHarnessSuite  extends AdvancedHarnessSuite {
+class AdvancedExtensionHarnessSuite extends AdvancedHarnessSuite {
 }
